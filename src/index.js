f950d60f5d4a46a189caf0839b70f0e75eed8483
Alex Merecka <merecka@gmail.com>
2019-10-13 10:40:14 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = "https://dog.ceo/api/breeds/list/all"
 
+
 function addImages(json) {
 	const image_container = document.querySelector('#dog-image-container')
 	json["message"].forEach(img => {
@@ -18,6 +19,7 @@ function addBreeds(breeds) {
 		const new_breed = document.createElement('li')		
 		new_breed.innerText = breed
 		breed_list.appendChild(new_breed)
+
 		new_breed.addEventListener("click", function(e){
 		console.log(e.target)
 		e.target.style.color = "#0000ff"
@@ -31,6 +33,8 @@ function addBreeds(breeds) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
+  const drop_down = document.getElementById('breed-dropdown')
+  let letter = drop_down.options[drop_down.selectedIndex].value
   fetch(imgUrl)
   .then(function(response) {
 	return response.json()
@@ -44,10 +48,31 @@ document.addEventListener('DOMContentLoaded', function() {
 	return response.json()
 	})
   .then(function(json) {
-  	breed_keys = Object.keys(json["message"])
+  	let breed_keys = Object.keys(json["message"])
 	addBreeds(breed_keys)
 	})
 })
+
+// Filters the breed list based on selected letter from drop down box
+document.addEventListener('input', function(event){
+	const breed_list = document.querySelector('#dog-breeds')
+	if (event.target.id !== 'breed-dropdown') return;
+	let letter = event.target.value
+	fetch(breedUrl)
+	  .then(function(response) {
+		return response.json()
+		})
+	  .then(function(json) {
+	  	let breed_keys = Object.keys(json["message"])
+		  	if (letter) {
+		  		breed_list.innerHTML = ""
+		  		let breeds = breed_keys.filter(breed => breed.startsWith(letter))
+		  		addBreeds(breeds)
+		  	} else {
+		  		addBreeds(breed_keys)
+		  	}
+		})
+}, false)
 
 
 	
